ac16e05d75202d0f952350f280dff76126f9f422
nakato156 <christianvelasces@gmail.com>
2024-03-28 14:42:46 -0500

    implementado flask

---
+++ app/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from .routes import pb_routes, api_routes
+
+def create_app():
+    app = Flask(__name__)
+
+    app.register_blueprint(pb_routes, url_prefix='/')
+    app.register_blueprint(api_routes, url_prefix='/api')
+
+    return app
